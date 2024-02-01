b6c9f8b75ae338d5a240e9eb110fb8268e4462b6
Chinnapatz <mr.chronos038@gmail.com>
2024-02-01 14:06:11 +0700

    Create validate customerID - Close #3

--- backend/entity/b6405977_negative_customer_id_test.go
+++ backend/entity/b6405977_negative_customer_id_test.go
@@ -8,20 +8,20 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestCustomer(t *testing.T)  {
+func TestCustomerID(t *testing.T)  {
 	g := NewWithT(t)
-	t.Run(`case incorrect age`, func (t *testing.T)  {
+	t.Run(`case incorrect customerID`, func (t *testing.T)  {
 		customer := entity.Customers {
 			Name: "best",
-			Age: "12.5",
-			CustomerID: "CM12345678",
+			Age: "12",
+			CustomerID: "AD12345678",
 		}
 		ok,err := govalidator.ValidateStruct(customer)
 
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).NotTo(BeNil())
 
-		g.Expect(err.Error()).To(Equal("Age is not interger"))
+		g.Expect(err.Error()).To(Equal("Invalid customerID"))
 	})
 
 	
--- backend/entity/customer.go
+++ backend/entity/customer.go
@@ -6,5 +6,5 @@ type Customers struct {
 	gorm.Model
 	Name		string  
 	Age			string		`valid:"int~Age is not interger"`
-	CustomerID 	string		`valid:"matches(^[C][UM]\\d{8}$)"`
+	CustomerID 	string		`valid:"matches(^[C][UM]\\d{8}$)~Invalid customerID"`
 }
